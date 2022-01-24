55ed91a701de53761d5783e8626f794fac658742
binayak4820 <binayakgiri4820@gmail.com>
2022-01-24 17:40:02 +0545

    Database Connection

--- index.php
+++ index.php
@@ -2,17 +2,18 @@
 
 require 'function.php';
 
+$pdo = connection();
+
+dd($pdo);
+
+
+
 $newArray = [
 	'age' => 27,
 	'HairColor' => "black",
 	'Name' => "Binayak",
 	'collection' => ['firstorder' => 'first','seconorder' => 'second']
 ];
-$a = '**';
-
-dd(preg_match('/([A-z]...)*/', $a)); //boolea
-
-preg_match_all(pattern, subject, matches)
 
 
 // require 'index-view.php';
